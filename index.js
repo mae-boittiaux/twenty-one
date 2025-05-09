6117eebc7d2330716aac87a9dfb8826296e99ee4
mae-boittiaux <maeboittiaux@gmail.com>
2025-05-09 18:22:51 +0100

    feat(option-buttons): on-click now appends choice to diaglogue statement

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const optionOne = document.getElementById('option-one');
 const optionTwo = document.getElementById('option-two');
 const optionThree = document.getElementById('option-three');
 
+const output = document.getElementById('output');
+
 optionOne.textContent = '1';
 optionTwo.textContent = '2';
 optionThree.textContent = '3';
+
+optionOne.onclick = () => {
+    output.append(optionOne.textContent);
+};
+optionTwo.onclick = () => {
+    output.append(optionTwo.textContent);
+};
+optionThree.onclick = () => {
+    output.append(optionThree.textContent);
+};
