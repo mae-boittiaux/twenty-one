0d4cf03b24676787ad2c3cc5f3d0f3f6fc4c08fe
mae-boittiaux <maeboittiaux@gmail.com>
2025-05-09 17:52:31 +0100

    feat(option-buttons): initialise interaction choice buttons with dialogue

---
+++ index.js
@@ -0,0 +1,7 @@
+const optionOne = document.getElementById('option-one');
+const optionTwo = document.getElementById('option-two');
+const optionThree = document.getElementById('option-three');
+
+optionOne.textContent = '1';
+optionTwo.textContent = '2';
+optionThree.textContent = '3';
