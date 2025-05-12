c9884b6cc360e81853154d4c8818b2ab640cb4b6
mae-boittiaux <maeboittiaux@gmail.com>
2025-05-12 22:50:12 +0100

    feat(index.js): result determined with message displayed when either player loses game

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const optionButtons = document.querySelectorAll('.option-button');
 
 let total = 0;
 
+const outputUser = 'user-output';
+const outputComputer = 'computer-output';
+
 setOptionButtonText();
 
 optionButtons.forEach(button => {
     button.onclick = () => {
-        const div = createDiv('user-output', (total += (Number(button.textContent) - total)));
-        output.append(div);
+        const choiceUser = calculateChoice((Number(button.textContent) - total));
+        playTurn(outputUser, choiceUser);
+        determineResult(choiceUser, true);
 
-        const computerResponse = createDiv('computer-output', (total += getRandomInteger(1, 4)));
-        output.append(computerResponse);
+        const choiceComputer = calculateChoice(getRandomInteger(1, 4));
+        playTurn(outputComputer, choiceComputer);
+        determineResult(choiceComputer, false);
+
         setOptionButtonText();
     };
 });
@@ -35,6 +41,25 @@ function createDiv(className, textContent) {
     return div;
 }
 
+function playTurn(playerOutput, playerChoice) {
+    const div = createDiv(playerOutput, playerChoice);
+    output.append(div);
+}
+
+function calculateChoice(playerChoice) {
+    return total += playerChoice;
+}
+
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function determineResult(choice, isUserTurn) {
+    if (choice >= 21) {
+        if (isUserTurn) {
+            playTurn(outputUser, 'I lose');
+        } else {
+            playTurn(outputComputer, 'You win');
+        }
+    }
 }
